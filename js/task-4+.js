03f257219b30e11bbc7b8a88dcc825228e79d3b1
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-05-13 22:28:19 +0200

    002

--- js/task-4+.js
+++ js/task-4+.js
@@ -101,18 +101,182 @@
 // Пройдись по отриманих значеннях за допомогою циклу та додай кожне значення до змінної totalSalary.
 // Поверни totalSalary як результат.
 
-function countTotalSalary(salaries){
-  let totalSalary = 0;
-  const salaryValues = Object.values(salaries);
-  for (let i = 0; i < salaryValues.length; i++){
-    totalSalary += salaryValues[i];
+// function countTotalSalary(salaries){
+//   let totalSalary = 0;
+//   const salaryValues = Object.values(salaries);
+//   for (let i = 0; i < salaryValues.length; i++){
+//     totalSalary += salaryValues[i];
+//   }
+//   return totalSalary;
+// }
+
+// console.log(countTotalSalary({}));
+// // повертає 0
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+// // повертає 330
+// console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+// // повертає 400
+
+// Масив colors містить колекцію кольорів. Кожен колір представлений об'єктом і має властивості hex і rgb з відповідними для цього формату і кольору значеннями.
+// Перебери масив об'єктів colors, використовуючи цикл for...of. Додай у масив hexColors значення властивостей hex, а в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (key of colors) {
+//   hexColors.push(key.hex);
+//   rgbColors.push(key.rgb);
+// }
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+// const books = [
+//   { title: "The Last Kingdom", author: "Bernard Cornwell" },
+//   { title: "Beside Still Waters", author: "Robert Sheckley" },
+//   { title: "The Dream of a Ridiculous Man", author: "Fyodor Dostoevsky" }
+// ];
+
+// const authorToSearchFor = "Robert Sheckley";
+
+// for (const book of books) {
+// 	if(book.author === authorToSearchFor) {
+// 		console.log(book);
+// 		// console.log(book.title)
+//     // console.log(book.rating)
+//     // console.log(book.author)
+// 	}
+// }
+
+// Функція getProductPrice(productName) приймає один параметр productName - назва продукту. Функція містить масив об'єктів products з такими властивостями, як name — ім'я товару, price — ціна і quantity — кількість.
+// Доповни код функції так, щоб вона шукала об'єкт продукту з певним ім'ям (властивість name) в масиві products і повертала його ціну (властивість price). Якщо продукт з такою назвою не знайдений, функція повинна повертати null.
+
+// function getProductPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+//   for (const product of products) {
+// 	if(product.name === productName) {
+//     return product.price;
+//    }
+//   }
+//   return null;
+// }
+
+// console.log(getProductPrice("Radar"));
+// // 1300
+// console.log(getProductPrice("Grip"));
+// // 1200
+
+// const books = [
+//   { title: "The Last Kingdom", author: "Bernard Cornwell", rating: 8.2 },
+//   { title: "Beside Still Waters", author: "Robert Sheckley", rating: 9 },
+//   { title: "The Dream of a Ridiculous Man", author: "Fyodor Dostoevsky", rating: 6.8 }
+// ];
+
+// const titles = [];
+
+// for (const book of books) {
+// 	titles.push(book.title)
+// }
+
+// console.log(titles); // ["The Last Kingdom", "Beside Still Waters", "The Dream of a Ridiculous Man"]
+
+// const books = [
+//   { title: "The Last Kingdom", author: "Bernard Cornwell", rating: 8.2 },
+//   { title: "Beside Still Waters", author: "Robert Sheckley", rating: 9 },
+//   { title: "The Dream of a Ridiculous Man", author: "Fyodor Dostoevsky", rating: 6.8 }
+// ];
+
+// let totalRating = 0;
+
+// for (const book of books) {
+//   totalRating += book.rating;
+// }
+
+// const averageRating = totalRating / books.length;
+// console.log(averageRating); // 8
+
+// Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості. Функція повинна повернути масив усіх значень властивості з таким ім'ям з кожного об'єкта в масиві products. Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.
+
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+//   const propValues = [];
+
+// for (const product of products) {
+// 	if(propName in product) {
+//     propValues.push(product[propName]);
+//    }
+//   }
+//   return propValues;
+// }
+
+// console.log(getAllPropValues("quantity"));
+// // [4, 3, 7, 9]
+// console.log(getAllPropValues("price"));
+// // [1300, 2700, 400, 1200]
+
+// Функція calculateTotalPrice(productName) приймає один параметр productName- назва товару. Функція містить масив об'єктів products з такими властивостями, як name — ім'я товару, price — ціна і quantity — кількість.
+
+// Доповни код функції так, щоб вона повертала загальну вартість (ціна * кількість) товару з таким ім'ям з масиву products.
+
+// Якщо продукту з такою назвою немає, то функція повинна повертати рядок "Product <productName> not found!" , де <productName> — це ім'я товару.
+
+function calculateTotalPrice(productName) {
+  const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+  ];
+
+  for (const product of products) {
+    if (product.name === productName) {
+      
+      return product.price * product.quantity;
+    }
   }
-  return totalSalary;
+ 
+  return `Product ${productName} not found!`;
+
+
+// let totalPrice = 0;
+// let productFound = false;
+
+// for (const product of products){
+//   if (product.name === productName) {
+//     totalPrice += product.price * product.quantity;
+//     productFound = true;  
+//   }
+//   }
+
+//   if (!productFound){
+//     return `Product ${productName} not found!`;
+//   }
+//   return totalPrice;
 }
 
-console.log(countTotalSalary({}));
-// повертає 0
-console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
-// повертає 330
-console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
-// повертає 400
+console.log(calculateTotalPrice("Blaster"));
+// "Product Blaster not found!"
+console.log(calculateTotalPrice("Radar"));
+// 5200
+console.log(calculateTotalPrice("Droid"));
+// 2800
